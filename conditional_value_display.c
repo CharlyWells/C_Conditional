84df5411f6ef9854458afecd01e0c9d450f1dfcd
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-11-15 18:55:23 +0000

    Added a new file

--- conditional_value_display.c
+++ conditional_value_display.c
@@ -7,5 +7,21 @@
 
 int main()
 {
+int m;
+printf("Type an integer: ");
+scanf("%d",&m);
+
+if(m>0)
+{
+  printf("The value of n = 1");
+}
+else if(m == 0)
+{
+  printf("The value of n = 0");
+}
+else
+{
+  printf("The value of n = -1");
+}
 return 0;
-)
+}
