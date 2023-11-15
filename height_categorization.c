15236ab4ac44522de3dc1f0220ed39e1e49ac66e
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-11-15 19:06:12 +0000

    Added a new file

---
+++ height_categorization.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * description: a C program to accept the height of a person in centimeters and categorize the person according to their height.
+ * written by: Charlotte Wellington
+ * return 0
+ */
+
+int main()
+{
+
+return 0;
+}
