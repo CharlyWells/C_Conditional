3b4f096e54b7e4e4f808925bae9bb853c125a040
CharlyWells <wellingtoncharlottenaaodarley@gmail.com>
2023-11-15 15:27:01 +0000

    Added a new file

---
+++ conditional_value_display.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * description -  a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.
+ * written by: Charlotte Wellington
+ * return 0
+ */
+
+int main()
+{
+return 0;
+)
